fb30aef700df13e43a9779801213c3194789651b
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-07-22 14:59:00 +0700

    test(fase-1): tambahkan test case aplikasi

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    // Bootstrap aplikasi menggunakan bootstrap/app.php bawaan Laravel 13.
+}
